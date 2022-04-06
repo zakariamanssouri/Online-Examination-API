7834082827b7a1872125440187555fc84ce2672b
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-06 22:38:57 +0000

    Update UserController

--- src/main/java/ma/enset/api/web/UserController.java
+++ src/main/java/ma/enset/api/web/UserController.java
@@ -1,68 +1,49 @@
 package ma.enset.api.web;
 
 import lombok.AllArgsConstructor;
-import ma.enset.api.Exceptions.UserNotFoundException;
 import ma.enset.api.entities.User;
-import ma.enset.api.repositories.UserRepository;
+import ma.enset.api.service.User.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @AllArgsConstructor
-@Transactional
 public class UserController {
-    private UserRepository userRepository;
+    private UserService userService;
 
 
     /*all users*/
     @GetMapping("/users")
     public List<User> getAllUsers() {
-        return userRepository.findAll();
+
+        return userService.getAll();
     }
 
-    @PostMapping("/user")
-    public String addNewUser(@Validated @RequestBody User user, BindingResult bindingResult) {
-        if (user.getUsername().equals("")) return "username cannot be null";
-        if (bindingResult.hasErrors()) {
-            return bindingResult.toString();
-        }
-        try {
-            userRepository.save(user);
-        } catch (Exception e) {
-            return e.getMessage();
-        }
-        return "success";
+    @PostMapping("/users")
+    ResponseEntity<String> addNewUser(@Valid @RequestBody User user) {
+        userService.addNewUser(user);
+        return ResponseEntity.ok("user created");
     }
 
 
     @GetMapping("/user/{id}")
     User getUser(@PathVariable long id) {
-        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+        return userService.getUserById(id);
     }
 
     @PutMapping("/user/{id}")
-    User updateUser(@RequestBody User newUser, @PathVariable long id) {
-        User oldUser = userRepository.findById(id).orElse(null);
-        if (oldUser!=null) {
-            String username = oldUser.getUsername().equals("")?newUser.getUsername():newUser.getUsername();
-            System.out.println(username);
-
-        } else throw new UserNotFoundException(id);
-        return userRepository.save(newUser);
+    User updateUser(@Valid @RequestBody User newUser, @PathVariable long id) {
+        return userService.updateUser(newUser, id);
     }
 
-
-
     @DeleteMapping("/user/{id}")
-    void deleteUser(@PathVariable long id) {
-        User user = userRepository.findById(id).orElse(null);
-        if (user == null) {
-            throw new UserNotFoundException(id);
-        } else userRepository.delete(user);
+    String deleteUser(@PathVariable long id) {
+        return userService.deleteUserById(id);
+
     }
 }
