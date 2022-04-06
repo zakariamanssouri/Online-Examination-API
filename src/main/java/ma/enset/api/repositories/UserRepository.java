de3905b6dd11edf31e764443b63f3e4f27809c2e
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-04-06 22:31:26 +0000

    add a getUserByUsername method

--- src/main/java/ma/enset/api/repositories/UserRepository.java
+++ src/main/java/ma/enset/api/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import ma.enset.api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String Username);
 }
