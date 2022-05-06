f4cf996ad44dea58bd999a613488c98951e8df9c
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-06 22:37:04 +0000

    add TestsExceptionsAdvice

---
+++ src/main/java/ma/enset/api/Exceptions/Tests/TestsExceptionsAdvice.java
@@ -0,0 +1,43 @@
+package ma.enset.api.Exceptions.Tests;
+
+import ma.enset.api.Exceptions.Users.UserAlreadyExistException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class TestsExceptionsAdvice {
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    String TestExists(TestAlreadyExistException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String TestNotFound(TestNotFoundException exception) {
+        return exception.getMessage();
+    }
+}
