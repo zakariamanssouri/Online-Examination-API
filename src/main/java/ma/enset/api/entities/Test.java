b523a98b07dc2e6a4722a11655873d5667f38350
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-06 22:33:38 +0000

    add JsonProperty for fixing cyclic dependency

--- src/main/java/ma/enset/api/entities/Test.java
+++ src/main/java/ma/enset/api/entities/Test.java
@@ -1,5 +1,6 @@
 package ma.enset.api.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,10 +20,12 @@ public class Test {
     private String code;
 
     @ManyToOne
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private User owner;
 
     @ManyToMany
     @Column(name = "user_id")
     @JoinTable(name = "passed_tests")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<User> users;
 }
