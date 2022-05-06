1690d94fddc11f11d00f09d47676c67513f009a6
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-06 22:35:10 +0000

    update TestServiceImp

--- src/main/java/ma/enset/api/service/Test/TestServiceImpl.java
+++ src/main/java/ma/enset/api/service/Test/TestServiceImpl.java
@@ -1,6 +1,8 @@
 package ma.enset.api.service.Test;
 
 import lombok.AllArgsConstructor;
+import ma.enset.api.Exceptions.Tests.TestAlreadyExistException;
+import ma.enset.api.Exceptions.Tests.TestNotFoundException;
 import ma.enset.api.entities.Test;
 import ma.enset.api.repositories.TestRepository;
 import org.springframework.stereotype.Service;
@@ -19,21 +21,39 @@ public class TestServiceImpl implements TestService {
 
     @Override
     public Test addNewTest(Test test) {
+        if(testRepository.findByCode(test.getCode())!=null) {
+            try {
+                throw new  TestAlreadyExistException();
+            } catch (TestAlreadyExistException e) {
+                e.printStackTrace();
+            }
+        }
         return testRepository.save(test);
     }
 
     @Override
     public Test getTestById(long id) {
-        return null;
+        if (testRepository.findById(id) == null) {
+            throw new TestNotFoundException();
+        }
+        return testRepository.findById(id);
     }
 
     @Override
     public Test updateTest(Test test, long id) {
-        return null;
+        test.setId(id);
+        return testRepository.save(test);
     }
 
     @Override
     public String deleteTestById(long id) {
-        return null;
+        if (testRepository.findById(id) == null) {
+            throw new TestNotFoundException();
+        }
+        else  {
+            testRepository.deleteById(id);
+            return "success";
+        }
+
     }
 }
