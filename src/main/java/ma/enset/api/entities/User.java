e6424cb6dbbc9ca1cc3acddd3ddc83051d5ab267
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-05-16 12:31:01 +0100

    fix mappedBy in relationship

--- src/main/java/ma/enset/api/entities/User.java
+++ src/main/java/ma/enset/api/entities/User.java
@@ -24,7 +24,7 @@ public class User {
     @NotBlank(message = "cannot be null")
     private String password;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "owner")
     private  List<Test> ownedTests;
 
     @ManyToMany(mappedBy = "users")
