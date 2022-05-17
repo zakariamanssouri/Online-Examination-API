c2a8a3a160ef56c9715920a2e81b9ad99b3f7169
mouad01laayouni <mouad01laayouni@gmail.com>
2022-05-17 13:40:46 +0100

    more small changes

--- src/main/java/ma/enset/api/ApiApplication.java
+++ src/main/java/ma/enset/api/ApiApplication.java
@@ -1,13 +1,7 @@
 package ma.enset.api;
 
-import ma.enset.api.entities.User;
-import ma.enset.api.repositories.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ApiApplication {
