b5d42929fbee994cb6378e60c18205476f9e0a70
zakariamanssouri <z.mansouri_etu@enset-media.ac.ma>
2022-06-11 22:00:41 +0100

    add findByNameContaining method in TestRepository

--- src/main/java/ma/enset/api/repositories/TestRepository.java
+++ src/main/java/ma/enset/api/repositories/TestRepository.java
@@ -2,8 +2,14 @@ package ma.enset.api.repositories;
 
 import ma.enset.api.entities.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TestRepository extends JpaRepository<Test,Long>{
     Test findByCode(String string);
     Test findById(long id);
+
+    @Query(value = "select t from Test t where t.name like %:keyword%")
+    List<Test> findByNameContaining(String keyword);
 }
